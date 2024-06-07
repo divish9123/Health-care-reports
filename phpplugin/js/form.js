f453ec77691cf888a50d5484d247d4177e3bc8bb
divish9123 <divishkumar6@gmail.com>
2024-06-07 23:56:17 +0530

    Add files via upload

---
+++ phpplugin/js/form.js
@@ -0,0 +1,12 @@
+Import { vid } from './uid';
+
+const id uid();
+
+export const uid = () => {
+
+const head = Date.now().toString(36);
+
+const tail = Math.random().toString(36).substr(2);
+
+return head + tail;
+}
